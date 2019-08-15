ea94be33d9c4ca4b502dafb8123223271b8382c6
masx200 <34191203+masx200@users.noreply.github.com>
2019-08-15 12:11:23 +0800

    优化

--- vue-simple-global-state-store-manager.js
+++ vue-simple-global-state-store-manager.js
@@ -1,10 +1,7 @@
 import Vue from "vue/dist/vue.esm.browser.js";
 
-
-
 const temptarget = new EventTarget();
 const simpleglobalstatestore = {};
-
 
 function newobjjson(obj) {
   if (typeof obj !== "object") {
@@ -19,56 +16,43 @@ function isobject(o) {
     o.__proto__ === Object.prototype
   );
 }
-export default function(组件状态对应全局状态表, vueinitopt) {
-  if (!isobject(组件状态对应全局状态表)) {
+export default function(组件状态对应全局状态表opt, vueinitopt) {
+  if (!isobject(组件状态对应全局状态表opt)) {
     throw Error("invalid object");
   }
-  const 组件状态对应全局状态表 = newobjjson(组件状态对应全局状态表);
-Object.keys(组件状态对应全局状态表).forEach((k)=>{
+  const 组件状态对应全局状态表 = newobjjson(组件状态对应全局状态表opt);
+  Object.keys(组件状态对应全局状态表).forEach(k => {
+    if (
+      //typeof k==="symbol"||
 
-if(
+      String(k).startsWith("_") ||
+      String(k).startsWith("$")
+    ) {
+      throw new TypeError("invalid key");
+    }
+  });
 
-//typeof k==="symbol"||
+  Object.values(组件状态对应全局状态表).forEach(v => {
+    if (
+      typeof v !== "string"
+      //||
 
-
-String(k).startsWith("_")||String(k).startsWith("$")){
-
-throw new TypeError("invalid key")
-
-}
-
-
-})
-
-Object.values(组件状态对应全局状态表).forEach((v)=>{
-
-if(
-
-typeof v!=="string"
-//||
-
-
-//String(k).startsWith("_")||String(k).startsWith("$")
-
-){
-
-throw new TypeError("invalid value")
-
-}
-
-
-})
-//Vue.extend自动识别是函数还是参数对象
+      //String(k).startsWith("_")||String(k).startsWith("$")
+    ) {
+      throw new TypeError("invalid value");
+    }
+  });
+  //Vue.extend自动识别是函数还是参数对象
 
   var vueinitconstructfun = Vue.extend(vueinitopt);
-com.prototype  =vueinitconstructfun.prototype
-return com
+  com.prototype = vueinitconstructfun.prototype;
+  return com;
 
-function com(o) {
+  function com(o) {
     var i = new Proxy(Object.create(vueinitconstructfun.prototype), {
       set(t, p, v) {
         Reflect.set(t, p, v);
-        if (p in ((组件状态对应全局状态表))) {
+        if (p in 组件状态对应全局状态表) {
           console.log(t, p, v);
         }
 
@@ -77,5 +61,5 @@ function com(o) {
     });
     vueinitconstructfun.call(i, o);
     return i;
-  };
+  }
 }
