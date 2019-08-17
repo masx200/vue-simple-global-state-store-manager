db1952cf5302f43ca43f294ac0af0466e3a566c9
masx200 <34191203+masx200@users.noreply.github.com>
2019-08-17 13:41:01 +0800

    Update vue-simple-global-state-store-manager.js

--- vue-simple-global-state-store-manager.js
+++ vue-simple-global-state-store-manager.js
@@ -16,63 +16,53 @@ function isobject(o) {
     o.__proto__ === Object.prototype
   );
 }
-export  function bindGlobalStore(jsonobjopt, vueinitopt) {
+export function bindGlobalStore(jsonobjopt, vueinitopt) {
   if (!isobject(jsonobjopt)) {
     throw Error("invalid object");
   }
 
-
-
-if(!("function"==typeof vueinitopt||"object"==typeof vueinitopt)){
-throw Error("invalid component");
-}
+  if (!("function" == typeof vueinitopt || "object" == typeof vueinitopt)) {
+    throw Error("invalid component");
+  }
 
   const 全局状态对应组件状态表 = newobjjson(jsonobjopt);
 
   Object.values(全局状态对应组件状态表).forEach(v => {
     if (
-      typeof v !== "string"
-      ||
-            String(v).startsWith("_") ||
+      typeof v !== "string" ||
+      String(v).startsWith("_") ||
       String(v).startsWith("$")
-
     ) {
       throw new TypeError("invalid value");
     }
-    
-    })
-    
+  });
+
   //Vue.extend自动识别是组件构造函数函数还是参数对象
 
   var vueinitconstructfun = Vue.extend(vueinitopt);
   com.prototype = vueinitconstructfun.prototype;
-  
-  
-  
-  
-   Object.keys(com).forEach(k => {
-com[k]=vueinitconstructfun[k]
+
+  Object.keys(com).forEach(k => {
+    com[k] = vueinitconstructfun[k];
   });
-  
-  
+
   return com;
 
   function com(o) {
-Object.keys(全局状态对应组件状态表).forEach(key => {
-     const eventname = "globalstatechange-" + key;
-    //
-    })
-function onmounted(){}
-function ondestroyed(){}
+    Object.keys(全局状态对应组件状态表).forEach(key => {
+      const eventname = "globalstatechange-" + key;
+      //
+    });
+    function onmounted() {}
+    function ondestroyed() {}
     var i = new Proxy(Object.create(vueinitconstructfun.prototype), {
       set(t, p, v) {
         Reflect.set(t, p, v);
         if (Object.values(全局状态对应组件状态表).includes(p)) {
           console.log(t, p, v);
+        } else {
+          console.log(t, p, v);
         }
-else{
-console.log(t, p, v);
-}
         return true;
       }
     });
@@ -89,7 +79,6 @@ export function initGlobalState(jsonobject) {
   const newobjtoreturn = {};
 
   Object.keys(newjsonobj).forEach(key => {
-
     if ("undefined" === typeof simpleglobalstatestore[key]) {
       simpleglobalstatestore[key] = newjsonobj[key];
     }
